0d5ea063f938d434f3e18cd53cf93aa4757efc87
luisdingww-bit <luisdingww-bit@users.noreply.github.com>
2026-07-28 15:49:14 +0800

    Update copyright text in CopyrightBar

--- src/components/CopyrightBar.tsx
+++ src/components/CopyrightBar.tsx
@@ -2,7 +2,7 @@ export default function CopyrightBar() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between px-6 py-4 text-sm text-[#051A24]">
       <span>Louis Ding</span>
-      <span>Copyright© 2026 louis ding</span>
+      <span>Copyright (c) 2026 Louis Ding (JunHui Ding)</span>
     </div>
   );
 }
